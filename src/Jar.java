fbfdc6d453fcb1d5a208119611bec2f5a1ff30ef
doberdanvsakdan <guppotf2@hotmail.com>
2021-05-24 16:30:48 +0200

    Pošiljam ris_jar.zip

--- src/Jar.java
+++
@@ -1,7 +0,0 @@
-import app.Main;
-
-public class Jar {
-    public static void main(String[] args) {
-        Main.main(args);
-    }
-}
---
+++ src/Main_1.java
@@ -0,0 +1,7 @@
+import app.Main;
+
+public class Main_1 {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
